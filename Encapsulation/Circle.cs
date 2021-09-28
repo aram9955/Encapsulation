dab1bf5a80c77646ed2dd0df264592777a0ca0a1
Aram Baghdasarian <aram99551@gmail.com>
2021-09-28 15:43:03 +0400

    Add project files.

---
+++ Encapsulation/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    public class Circle
+    {
+        public Circle(int r)
+        {
+            _r = r;
+        }
+        private int _r;
+        public int R
+        {
+            get
+            {
+                return _r;
+            }
+            set
+            {
+
+                {
+                    if (value > 100)
+                        _r = value;
+                }
+            }
+        }
+    }
+}
